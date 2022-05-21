862de5da73a2777ddd6af80d755841437bafbf47
clement-gambier2 <78157383+clement-gambier2@users.noreply.github.com>
2022-05-21 08:28:29 +0200

    remove and do it in react

--- index.js
+++
@@ -1,9 +0,0 @@
-function getValue() {
-    // Sélectionner l'élément input et récupérer sa valeur
-    let input = document.getElementById("writer").value;
-    // Afficher la valeur
-    console.log(input);
-
-    let reader = document.getElementById("reader");
-    reader.innerHTML = input;
-}
